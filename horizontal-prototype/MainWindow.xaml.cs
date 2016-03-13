245e58cc9fca5407000ef370d53058c269c0fe83
clam12 <clam12.cl@gmail.com>
2016-03-12 17:33:52 -0700

    button functionality works, need to add content to movies, settings, tv shows and musics. Also need to apply changes to after login and then maybe check for username and password

--- horizontal-prototype/MainWindow.xaml.cs
+++ horizontal-prototype/MainWindow.xaml.cs
@@ -27,42 +27,59 @@ namespace horizontal_prototype
 
         private void netflix_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                System.Diagnostics.Process.Start("https://www.netflix.com/Login?locale=en-CA");
+            }
+            catch { }
         }
 
         private void movies_Click(object sender, RoutedEventArgs e)
         {
-
+            movieList.Visibility = Visibility.Visible;
+            movieListBack.Visibility = Visibility.Visible;
         }
 
         private void favourites_button_Click(object sender, RoutedEventArgs e)
         {
-
+            favoritesList.Visibility = Visibility.Visible;
+            favoritesListBack.Visibility = Visibility.Visible;
         }
 
         private void music_button_Click(object sender, RoutedEventArgs e)
         {
-
+            musicList.Visibility = Visibility.Visible;
+            musicListBack.Visibility = Visibility.Visible;
         }
 
         private void tvshows_button_Click(object sender, RoutedEventArgs e)
         {
-
+            tvshowList.Visibility = Visibility.Visible;
+            tvshowListBack.Visibility = Visibility.Visible;
         }
 
         private void settings_button_Click(object sender, RoutedEventArgs e)
         {
-
+            settings.Visibility = Visibility.Visible;
+            settingsBack.Visibility = Visibility.Visible;
         }
 
         private void youtube_button_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/");
+            }
+            catch { }
         }
 
         private void spotify_button_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                System.Diagnostics.Process.Start("https://accounts.spotify.com/en/login?continue=https:%2F%2Fwww.spotify.com%2Fca-en%2Faccount%2Foverview%2F");
+            }
+            catch { }
         }
 
         private void Username_form_TextChanged(object sender, TextChangedEventArgs e)
@@ -70,6 +87,48 @@ namespace horizontal_prototype
 
         }
 
+        private void Password_form_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
+        }
+
+        private void Login_button_Click(object sender, RoutedEventArgs e)
+        {
+            Login_button.Visibility = Visibility.Hidden;
+            Login.Visibility = Visibility.Hidden;
+            Username_form.Visibility = Visibility.Hidden;
+            Password.Visibility = Visibility.Hidden;
+            Password_form.Visibility = Visibility.Hidden;
+        }
+
+        private void closeMovieList(object sender, RoutedEventArgs e)
+        {
+            movieList.Visibility = Visibility.Hidden;
+            movieListBack.Visibility = Visibility.Hidden;
+        }
+
+        private void closeTvList(object sender, RoutedEventArgs e)
+        {
+            tvshowList.Visibility = Visibility.Hidden;
+            tvshowListBack.Visibility = Visibility.Hidden;
+        }
+
+        private void closeFavoritesList(object sender, RoutedEventArgs e)
+        {
+            favoritesList.Visibility = Visibility.Hidden;
+            favoritesListBack.Visibility = Visibility.Hidden;
+        }
+
+        private void closeMusicList(object sender, RoutedEventArgs e)
+        {
+            musicList.Visibility = Visibility.Hidden;
+            musicListBack.Visibility = Visibility.Hidden;
+        }
+
+        private void closeSettings(object sender, RoutedEventArgs e)
+        {
+            settings.Visibility = Visibility.Hidden;
+            settingsBack.Visibility = Visibility.Hidden;
+        }
     }
 }
