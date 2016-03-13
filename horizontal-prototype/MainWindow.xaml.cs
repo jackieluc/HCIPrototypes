d1649d3502cb16b7af1dbcea6ab6003d9106bbaf
clam12 <clam12.cl@gmail.com>
2016-03-12 19:11:42 -0700

    added titles for each extra screen and added setting buttons

--- horizontal-prototype/MainWindow.xaml.cs
+++ horizontal-prototype/MainWindow.xaml.cs
@@ -38,30 +38,39 @@ namespace horizontal_prototype
         {
             movieList.Visibility = Visibility.Visible;
             movieListBack.Visibility = Visibility.Visible;
+            movieTitle.Visibility = Visibility.Visible;
         }
 
         private void favourites_button_Click(object sender, RoutedEventArgs e)
         {
             favoritesList.Visibility = Visibility.Visible;
             favoritesListBack.Visibility = Visibility.Visible;
+            favoritesTitle.Visibility = Visibility.Visible;
         }
 
         private void music_button_Click(object sender, RoutedEventArgs e)
         {
             musicList.Visibility = Visibility.Visible;
             musicListBack.Visibility = Visibility.Visible;
+            musicTitle.Visibility = Visibility.Visible;
         }
 
         private void tvshows_button_Click(object sender, RoutedEventArgs e)
         {
             tvshowList.Visibility = Visibility.Visible;
             tvshowListBack.Visibility = Visibility.Visible;
+            tvshowTitle.Visibility = Visibility.Visible;
         }
 
         private void settings_button_Click(object sender, RoutedEventArgs e)
         {
             settings.Visibility = Visibility.Visible;
             settingsBack.Visibility = Visibility.Visible;
+            settingsTitle.Visibility = Visibility.Visible;
+            videoSettings.Visibility = Visibility.Visible;
+            powerSettings.Visibility = Visibility.Visible;
+            dataAndTime.Visibility = Visibility.Visible;
+            audioSettings.Visibility = Visibility.Visible;
         }
 
         private void youtube_button_Click(object sender, RoutedEventArgs e)
@@ -84,7 +93,7 @@ namespace horizontal_prototype
 
         private void Username_form_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            
         }
 
         private void Password_form_TextChanged(object sender, TextChangedEventArgs e)
@@ -94,41 +103,52 @@ namespace horizontal_prototype
 
         private void Login_button_Click(object sender, RoutedEventArgs e)
         {
+            
             Login_button.Visibility = Visibility.Hidden;
             Login.Visibility = Visibility.Hidden;
             Username_form.Visibility = Visibility.Hidden;
             Password.Visibility = Visibility.Hidden;
             Password_form.Visibility = Visibility.Hidden;
+            
         }
 
         private void closeMovieList(object sender, RoutedEventArgs e)
         {
             movieList.Visibility = Visibility.Hidden;
             movieListBack.Visibility = Visibility.Hidden;
+            movieTitle.Visibility = Visibility.Hidden;
         }
 
         private void closeTvList(object sender, RoutedEventArgs e)
         {
             tvshowList.Visibility = Visibility.Hidden;
             tvshowListBack.Visibility = Visibility.Hidden;
+            tvshowTitle.Visibility = Visibility.Hidden;
         }
 
         private void closeFavoritesList(object sender, RoutedEventArgs e)
         {
             favoritesList.Visibility = Visibility.Hidden;
             favoritesListBack.Visibility = Visibility.Hidden;
+            favoritesTitle.Visibility = Visibility.Hidden;
         }
 
         private void closeMusicList(object sender, RoutedEventArgs e)
         {
             musicList.Visibility = Visibility.Hidden;
             musicListBack.Visibility = Visibility.Hidden;
+            musicTitle.Visibility = Visibility.Hidden;
         }
 
         private void closeSettings(object sender, RoutedEventArgs e)
         {
             settings.Visibility = Visibility.Hidden;
             settingsBack.Visibility = Visibility.Hidden;
+            settingsTitle.Visibility = Visibility.Hidden;
+            videoSettings.Visibility = Visibility.Hidden;
+            powerSettings.Visibility = Visibility.Hidden;
+            dataAndTime.Visibility = Visibility.Hidden;
+            audioSettings.Visibility = Visibility.Hidden;
         }
     }
 }
